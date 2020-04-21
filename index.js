d0822ed6286dace087a6b1e8e0ab4a878c8b0f27
Jimmy <jimmystry@gmail.com>
2020-04-21 15:48:57 -0400

    Silly mistake! The start script referenced a server.js file, which doesn't exist. Changed to index.js. Hope it works!

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 
-const server = require("http").createServer(app);
-
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
@@ -11,5 +9,5 @@ app.get("/", (req, res) => {
     });
 });
 
-server.listen(port);
+server.listen(app);
 console.log("Listening on port 3000");
